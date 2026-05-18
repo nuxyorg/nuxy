872fba8b46bd457b86a9408fcc85f8381005c651
Ata Gülalan <gulalanata@gmail.com>
2026-05-18 20:35:44 +0300

    chore: remove stale build artifacts and distribution files from desktop directory

--- apps/desktop/dist-electron/main.js
+++
@@ -1,6 +0,0 @@
-import "electron";
-import "./main-6420747d.js";
-import "path";
-import "fs";
-import "os";
-import "worker_threads";
--- apps/desktop/dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("core", {
-  ipc: {
-    invoke: (extId, channel, payload) => electron.ipcRenderer.invoke("ext:invoke", extId, channel, payload)
-  }
-});
